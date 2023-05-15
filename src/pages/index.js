e284eb8a9698bd66255c7787be09928442c8c126
Jakub Blazewicz <jakub@caskshare.com>
2023-05-15 12:45:16 +0200

    enabled vide player

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,10 @@ export default function Home() {
     <>
       <Head>
         <title>John Hartson Recovery</title>
-        <meta name="keywords" content="gambling addiction, compulsive gambler, gambling help, gambling addiction help, gambling problem, gamblers in recovery, gambling addiction treatment, stop gambling, pathological gambling, betting addiction, casino addiction, stop gambling addiction, help to stop gambling, gambling addiction recovery, gambling problem help, gambling help number, ">
+        <meta
+          name="keywords"
+          content="gambling addiction, compulsive gambler, gambling help, gambling addiction help, gambling problem, gamblers in recovery, gambling addiction treatment, stop gambling, pathological gambling, betting addiction, casino addiction, stop gambling addiction, help to stop gambling, gambling addiction recovery, gambling problem help, gambling help number, "
+        />
         <meta
           name="description"
           content="John Hartson's Gambling and Addiction Workshop. Make your next game recovery!"
@@ -26,7 +29,10 @@ export default function Home() {
         <meta property="og:url" content="https://jhrocevory.co.uk/" />
         <meta property="og:type" content="website" />
         <link rel="icon" href="/favicon.ico" />
-        <meta name="google-site-verification" content="b2ndndZkZz7_TY_Vwjqn5_D7UiCvR8aSwav9v1HLKWY" />
+        <meta
+          name="google-site-verification"
+          content="b2ndndZkZz7_TY_Vwjqn5_D7UiCvR8aSwav9v1HLKWY"
+        />
         <html lang="en-GB"></html>
       </Head>
 
@@ -34,7 +40,7 @@ export default function Home() {
         <HomeComp />
         <OurMission />
         <OurTeam />
-        {/* <Testimonials /> */}
+        <Testimonials />
         <Contact />
       </main>
     </>
